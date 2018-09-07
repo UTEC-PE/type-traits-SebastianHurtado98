3bf4bc45451e18289a7e34aa5eb9e4c8f9073c3f
Sebastian Hurtado <sebastian.hurtado@utec.edu.pe>
2018-09-06 19:22:06 -0500

    Add files via upload

--- dlist.h
+++ dlist.h
@@ -1,61 +1,123 @@
-#ifndef DLIST_H
-#define DLIST_H
+//
+// Created by sebastian on 9/6/18.
+//
+
+#ifndef TYPE_TRAITS_DEVELOP_DLIST_H
+#define TYPE_TRAITS_DEVELOP_DLIST_H
+
+
 
 #include "iterator.h"
+#include <iostream>
+using namespace std;
 
 template <typename T>
-class DListIterator : public Iterator<T> {     
-    public: 
-        DListIterator() : Iterator<T>() {};
-        DListIterator(Node<T> *current) : Iterator<T>(current) {};
-        DListIterator<T> operator++();
-        DListIterator<T> operator--();
+class DListIterator : public Iterator<T> {
+public:
+    DListIterator() : Iterator<T>() {};
+    DListIterator(Node<T> *current) : Iterator<T>(current) {};
+    DListIterator<T> operator++(){
+        this->current= (this->current)->next;
+    };
+    DListIterator<T> operator--(){
+        this->current= (this->current)->prev;
+    };
 };
 
 template <typename Tr>
-class DList {     
-    public:
-        typedef typename Tr::T T;
-        typedef DListIterator<T> iterator;
-      
-    private:
-        Node<T>* head;
-        Node<T>* tail;
-              
-    public:
-        DList() {
-            head = tail = nullptr;
-        };
+class DList {
+public:
+    typedef typename Tr::T T;
+    typedef DListIterator<T> iterator;
 
-        void push_front(T data) {
-            // TODO
+private:
+    Node<T>* head;
+    Node<T>* tail;
+
+public:
+    DList() {
+        head = tail = nullptr;
+    };
+
+    void push_front(T data) {
+        auto *n2= new Node<T>(data);
+        if (head== nullptr){
+            n2->next= nullptr;
+            n2->prev= nullptr;
+            head= n2;
+            tail=n2;
+        }
+        else{
+            n2->next= head;
+            head->prev= n2;
+            n2->prev= nullptr;
+            head= n2;
+        }
+    }
+
+    void print(){
+        auto temp=head;
+        while (temp!= nullptr){
+            cout << temp->data << endl;
+            temp= temp->next;
         }
 
-        void push_back(T data) {
-            // TODO
+    }
+
+    void push_back(T data) {
+        auto *n2= new Node<T>(data);
+        if (head== nullptr){
+            n2->next= nullptr;
+            n2->prev= nullptr;
+            head= n2;
+            tail= n2;
         }
-             
-        void pop_front() {
-            // TODO
+        else{
+            n2->prev= tail;
+            tail->next= n2;
+            n2->next= nullptr;
+            tail= n2;
         }
-             
-        void pop_back() {
-            // TODO
+    }
+
+    void pop_front() {
+        if (head== nullptr){
+            cout << "Lista vacia" << endl;
         }
-             
-        iterator begin() {
-            // TODO
+        else {
+            auto temp = head;
+            head = head->next;
+            head->prev = nullptr;
+            delete temp;
         }
-             
-        iterator end() {
-            // TODO
+    }
+    void pop_back() {
+        if (head== nullptr){
+            cout << "Lista vacia" << endl;
         }
-             
-        ~DList() {
-            if (head) {
-                head->killSelf();
-            } 
-        }         
+        else {
+            auto temp = tail;
+            tail = tail->prev;
+            tail->next= nullptr;
+            delete temp;
+        }
+    }
+
+    iterator begin() {
+        return iterator(head);
+    }
+
+    iterator end() {
+
+        return iterator(tail);
+    }
+
+    ~DList() {
+        if (head) {
+            head->killSelf();
+        }
+    }
 };
 
-#endif
+
+#endif //TYPE_TRAITS_DEVELOP_DLIST_H
--- iterator.h
+++ iterator.h
@@ -1,20 +1,29 @@
-#ifndef ITERATOR_H
-#define ITERATOR_H
+//
+// Created by sebastian on 9/6/18.
+//
 
+#ifndef TYPE_TRAITS_DEVELOP_ITERATOR_H
+#define TYPE_TRAITS_DEVELOP_ITERATOR_H
 #include "node.h"
 
 template <typename T>
 class Iterator {
-    protected:
-        Node<T>* current;
-      
-    public:
-        Iterator() : current(nullptr) {};
-        Iterator(Node<T>* current) : current(current) {};
-             
-        Iterator<T> operator=(Iterator<T> other);
-        bool operator!=(Iterator<T> other);
-        T operator*();
-};  
+protected:
+    Node<T>* current;
 
-#endif
+public:
+    Iterator() : current(nullptr) {};
+    Iterator(Node<T>* current) : current(current) {};
+
+    Iterator<T> operator=(Iterator<T> other){
+        current= other.current;
+        return other;
+    };
+    bool operator!=(Iterator<T> other){
+        return current!=other.current;
+    };
+    T operator*(){
+        return current->data;
+    };
+};
+#endif //TYPE_TRAITS_DEVELOP_ITERATOR_H
